150c35127ffb88eeefb74c34db19e9b5ddbe3493
Practical-Data <kiranfatima013@gmail.com>
2022-11-23 17:25:28 +0500

    react-projects

---
+++ React-Projects/challange3(fetchAPI)/Post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <button className='post'>post</button>
+  )
+}
+
+export default Post
---
+++ React-Projects/challange3(fetchAPI)/User.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <button className='user'>users</button>
+  )
+}
+
+export default User
